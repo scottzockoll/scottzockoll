51935ab398ae99ad0878bf9f06a64ea199166dc9
scottzockoll <zockoll.scott@gmail.com>
2025-09-21 18:34:52 -0700

    rename

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@scottzockoll/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
